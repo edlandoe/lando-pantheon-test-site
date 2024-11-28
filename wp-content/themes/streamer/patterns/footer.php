ebb0b9e13cb5c50883b86c9b8f933545f1dda701
Edlando Eliacin <landoeliacin@gmail.com>
2024-11-28 22:14:11 +0000

    addded theme

---
+++ wp-content/themes/streamer/patterns/footer.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * Title: footer
+ * Slug: streamer/footer
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"metadata":{"name":"Footer wrapper"},"style":{"border":{"top":{"color":"var:preset|color|primary","width":"1px"},"right":{"width":"0px","style":"none"},"bottom":{"width":"0px","style":"none"},"left":{"width":"0px","style":"none"}},"spacing":{"padding":{"right":"var:preset|spacing|60","left":"var:preset|spacing|60","top":"0","bottom":"var:preset|spacing|80"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="border-top-color:var(--wp--preset--color--primary);border-top-width:1px;border-right-style:none;border-right-width:0px;border-bottom-style:none;border-bottom-width:0px;border-left-style:none;border-left-width:0px;padding-top:0;padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--60)"><!-- wp:group {"metadata":{"name":"Links"},"align":"full","style":{"spacing":{"blockGap":"0"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group alignfull"><!-- wp:columns {"verticalAlignment":"center","align":"full","style":{"border":{"bottom":{"color":"var:preset|color|background","width":"1px"},"top":[],"right":[],"left":[]},"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"},"blockGap":{"top":"var:preset|spacing|20"}}}} -->
+<div class="wp-block-columns alignfull are-vertically-aligned-center" style="border-bottom-color:var(--wp--preset--color--background);border-bottom-width:1px;padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:column {"verticalAlignment":"center","width":"33.3%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:33.3%"><!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading"><?php esc_html_e('Follow us on Twitch', 'streamer');?></h3>
+<!-- /wp:heading --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","width":"66.6%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:66.6%"><!-- wp:group {"metadata":{"name":"Detail line"},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":3,"style":{"elements":{"link":{"color":{"text":"var:preset|color|tertiary"}}}},"textColor":"tertiary"} -->
+<h3 class="wp-block-heading has-tertiary-color has-text-color has-link-color"><?php esc_html_e('@chronowarriors', 'streamer');?></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|tertiary"}}}},"textColor":"tertiary","fontSize":"small"} -->
+<p class="has-tertiary-color has-text-color has-link-color has-small-font-size"><?php esc_html_e('↗', 'streamer');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns {"verticalAlignment":"center","align":"full","style":{"border":{"bottom":{"color":"var:preset|color|background","width":"1px"},"top":[],"right":[],"left":[]},"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"},"blockGap":{"top":"var:preset|spacing|20"}}}} -->
+<div class="wp-block-columns alignfull are-vertically-aligned-center" style="border-bottom-color:var(--wp--preset--color--background);border-bottom-width:1px;padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:column {"verticalAlignment":"center","width":"33.3%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:33.3%"><!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading"><?php esc_html_e('E-mail', 'streamer');?></h3>
+<!-- /wp:heading --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","width":"66.6%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:66.6%"><!-- wp:group {"metadata":{"name":"Detail line"},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":3,"style":{"elements":{"link":{"color":{"text":"var:preset|color|tertiary"}}}},"textColor":"tertiary"} -->
+<h3 class="wp-block-heading has-tertiary-color has-text-color has-link-color"><?php esc_html_e('info@chronowarriorsteam.com', 'streamer');?></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|tertiary"}}}},"textColor":"tertiary","fontSize":"small"} -->
+<p class="has-tertiary-color has-text-color has-link-color has-small-font-size"><?php esc_html_e('↗', 'streamer');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns {"verticalAlignment":"center","align":"full","style":{"border":{"bottom":{"color":"var:preset|color|background","width":"1px"},"top":[],"right":[],"left":[]},"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"},"blockGap":{"top":"var:preset|spacing|20"}}}} -->
+<div class="wp-block-columns alignfull are-vertically-aligned-center" style="border-bottom-color:var(--wp--preset--color--background);border-bottom-width:1px;padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:column {"verticalAlignment":"center","width":"33.3%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:33.3%"><!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading"><?php esc_html_e('Social', 'streamer');?></h3>
+<!-- /wp:heading --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","width":"66.6%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:66.6%"><!-- wp:group {"metadata":{"name":"Detail line"},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:group {"metadata":{"name":"Links"},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":3,"style":{"elements":{"link":{"color":{"text":"var:preset|color|tertiary"}}}},"textColor":"tertiary"} -->
+<h3 class="wp-block-heading has-tertiary-color has-text-color has-link-color"><?php esc_html_e('/tumblr', 'streamer');?></h3>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"level":3,"style":{"elements":{"link":{"color":{"text":"var:preset|color|tertiary"}}}},"textColor":"tertiary"} -->
+<h3 class="wp-block-heading has-tertiary-color has-text-color has-link-color"><?php esc_html_e('/Utube', 'streamer');?></h3>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"level":3,"style":{"elements":{"link":{"color":{"text":"var:preset|color|tertiary"}}}},"textColor":"tertiary"} -->
+<h3 class="wp-block-heading has-tertiary-color has-text-color has-link-color"><?php esc_html_e('/Instgr', 'streamer');?></h3>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|tertiary"}}}},"textColor":"tertiary","fontSize":"small"} -->
+<p class="has-tertiary-color has-text-color has-link-color has-small-font-size"><?php esc_html_e('↗', 'streamer');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"height":"var:preset|spacing|40"} -->
+<div style="height:var(--wp--preset--spacing--40)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:columns {"metadata":{"name":"Subscribe"},"align":"full"} -->
+<div class="wp-block-columns alignfull"><!-- wp:column {"width":"33.3%"} -->
+<div class="wp-block-column" style="flex-basis:33.3%"><!-- wp:heading {"level":1} -->
+<h1 class="wp-block-heading"><?php esc_html_e('Level Up Your Inbox', 'streamer');?></h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php esc_html_e('Don\'t miss a beat. Subscribe now for insider tips, updates, and exclusive offers.', 'streamer');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"66.6%"} -->
+<div class="wp-block-column" style="flex-basis:66.6%"><!-- wp:jetpack/contact-form {"subject":"[Streamer] Blog Home","to":"dev-email@wpengine.local"} -->
+<div class="wp-block-jetpack-contact-form">
+<!-- wp:jetpack/field-name {"required":true,"requiredText":"(required)","borderWidth":0,"labelFontSize":"x-small","fieldFontSize":"mediun","lineHeight":1.5,"labelLineHeight":1.5,"inputColor":"#d8fe01","fieldBackgroundColor":"#899ba91a"} /-->
+<!-- wp:jetpack/field-email {"required":true,"requiredText":"(required)","borderWidth":0,"labelFontSize":"x-small","fieldFontSize":"medium","lineHeight":1.5,"labelLineHeight":1.5,"inputColor":"#d8fe01","fieldBackgroundColor":"#899ba91a"} /-->
+<!-- wp:jetpack/field-consent {"requiredText":"(required)","borderWidth":0,"labelFontSize":"x-small","fieldFontSize":"medium","lineHeight":1.5,"labelLineHeight":1.5,"inputColor":"#d8fe01","fieldBackgroundColor":"#899ba91a","implicitConsentMessage":"You're giving us permission to email you. You may unsubscribe at any time."} /-->
+<!-- wp:jetpack/button {"element":"button","text":"Subscribe","textColor":"primary","backgroundColor":"tertiary","lock":{"remove":true},"style":{"typography":{"textTransform":"uppercase"}}} /-->
+</div>
+<!-- /wp:jetpack/contact-form --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:spacer {"height":"var:preset|spacing|40"} -->
+<div style="height:var(--wp--preset--spacing--40)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:columns {"metadata":{"name":"Designed with WordPress"},"align":"full"} -->
+<div class="wp-block-columns alignfull"><!-- wp:column {"width":"33.3%"} -->
+<div class="wp-block-column" style="flex-basis:33.3%"><!-- wp:paragraph {"align":"left","style":{"typography":{"textTransform":"uppercase"}},"fontSize":"small","fontFamily":"utara"} -->
+<p class="has-text-align-left has-utara-font-family has-small-font-size" style="text-transform:uppercase"><?php /* Translators: 1. is the start of a 'a' HTML element, 2. is the end of a 'a' HTML element */ 
+echo sprintf( esc_html__( 'Designed with %1$sWordPress%2$s', 'streamer' ), '<a rel="nofollow" href="' . esc_url( 'https://wordpress.org' ) . '">', '</a>' ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"66.6%"} -->
+<div class="wp-block-column" style="flex-basis:66.6%"></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
