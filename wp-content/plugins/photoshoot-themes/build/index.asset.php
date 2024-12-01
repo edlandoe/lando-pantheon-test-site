3500cf529bf0850b9a8b33dc93d3df255a1911d6
Edlando Eliacin <landoeliacin@gmail.com>
2024-12-01 15:29:26 -0500

    update build to add css  to block

--- wp-content/plugins/photoshoot-themes/build/index.asset.php
+++ wp-content/plugins/photoshoot-themes/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '5a1c691b4d8a58c083e2');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'facabe4fad04f38ef343');
