2c3f8bd68038f97e998084acb71afe7368c107fb
Edlando Eliacin <landoeliacin@gmail.com>
2024-12-01 14:21:38 -0500

    add plugin photoshoot-themes to site

---
+++ wp-content/plugins/photoshoot-themes/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '5a1c691b4d8a58c083e2');
---
+++ wp-content/plugins/photoshoot-themes/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '4e16c184b937a4df0fcc');
