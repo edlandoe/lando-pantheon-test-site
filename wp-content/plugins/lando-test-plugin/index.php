93a4aa2e755f0f3c778f8763bd41349def6cce17
Edlando Eliacin <landoeliacin@gmail.com>
2024-12-01 14:40:53 -0500

    update description to lando-test plugin

--- wp-content/plugins/lando-test-plugin/index.php
+++ wp-content/plugins/lando-test-plugin/index.php
@@ -2,6 +2,7 @@
 /*
 Plugin Name: Lando-Test-Plugin
 Plugin URI: http://akismet.com/
+Description: A test plugin to play around with.
 Version: 1.0.0
 Author: Edlando Eliacin
 Author URI: https://www.edlandoeliacin.com
